de0020b56e2a6373e457e37a0b8a25e426c659f4
weweaaa <a0926422280@gmail.com>
2020-02-06 17:57:26 +0800

    調整：路徑引用

--- src/app/core/list.service.ts
+++ src/app/core/list.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Task } from '../domain/Task';
 import { Subscribable, Observable, Subscription } from 'rxjs';
-import { environment } from 'src/environments/environment';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
